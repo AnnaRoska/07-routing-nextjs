222f627179b7714ec2e4a1ea56f45fa02835d4eb
AnnaRoska <murkesha2014@gmail.com>
2026-01-30 16:26:31 +0200

    Edit error

--- app/notes/filter/[...slug]/page.tsx
+++ app/notes/filter/[...slug]/page.tsx
@@ -2,13 +2,13 @@ import NotesClient from "../../Notes.client";
 
 interface Props {
   params: Promise<{
-    tag?: string[];
+    slug?: string[];
   }>;
 }
 export default async function FilteredNotesPage({ params }: Props) {
-  const { tag } = await params;
+  const { slug } = await params;
 
-  const currentTag = tag?.[0];
+  const currentTag = slug?.[0];
 
   return (
     <NotesClient tag={currentTag === "all" ? undefined : currentTag} />
